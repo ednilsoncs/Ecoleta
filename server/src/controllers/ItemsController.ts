abdd7c283c9e861cd853cfacd91fed29d0b51d7d
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-06-02 22:57:35 -0300

    feat: added itemsController

---
+++ server/src/controllers/ItemsController.ts
@@ -0,0 +1,19 @@
+import { Response, Request } from "express";
+import knex from "../database/connection";
+
+class ItemsController {
+  async index(request: Request, response: Response) {
+    const items = await knex("items").select("*");
+
+    const serializedItems = items.map((item) => {
+      return {
+        title: item.title,
+        item_url: `http://localhost:3333/uploads/${item.image}`,
+      };
+    });
+
+    return response.json(serializedItems);
+  }
+}
+
+export default ItemsController;
