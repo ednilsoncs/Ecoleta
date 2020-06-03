133d43110c7f289fccba345c54f2c58942aa2678
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-06-02 21:53:49 -0300

    feta: added post point and get image

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,54 @@
-import express from "express";
+import express, { request, response } from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/users", (request, response) => {
-  return response.json({ name: "ednilson" });
+routes.get("/items", async (request, response) => {
+  const items = await knex("items").select("*");
+
+  const serializedItems = items.map((item) => {
+    return {
+      title: item.title,
+      item_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return response.json(serializedItems);
+});
+
+routes.post("/points", async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+  const trx = await knex.transaction();
+  const insertedIds = await trx("points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+  const point_id = insertedIds[0];
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    };
+  });
+
+  await trx("point_items").insert(pointItems);
+
+  return response.json({ success: true });
 });
 
 export default routes;
