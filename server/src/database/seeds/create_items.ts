fdb732e1f24ff9391d99aa95b5adef9846a70308
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-06-02 21:53:24 -0300

    fix: seed the items

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -1,7 +1,7 @@
 import Knex from "knex";
 
 export async function seed(knex: Knex) {
-  knex("items").insert([
+  await knex("items").insert([
     { title: "Lâmpadas", image: "lampadas.svg" },
     { title: "Pilhas e Baterias", image: "baterias.svg" },
     { title: "Papéis e Papelão", image: "papeis-papelao.svg" },
