d5af948d42802a1360e5ffb0abbdeaaa151df104
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-06-03 00:01:03 -0300

    feat: adicionado id em item controler

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -7,6 +7,7 @@ class ItemsController {
 
     const serializedItems = items.map((item) => {
       return {
+        id: item.id,
         title: item.title,
         item_url: `http://localhost:3333/uploads/${item.image}`,
       };
