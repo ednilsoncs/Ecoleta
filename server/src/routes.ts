149533468cc11101dd023b45ec4c0fae79e8bc05
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-06-02 23:59:55 -0300

    feat:method point index

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import knex from "./database/connection";
 
 import PointsController from "./controllers/PointsController";
 import ItemsController from "./controllers/ItemsController";
@@ -9,7 +8,7 @@ const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get("/items", itemsController.index);
-
+routes.get("/points", pointsController.index);
 routes.post("/points", pointsController.create);
 routes.get("/points/:id", pointsController.show);
 
