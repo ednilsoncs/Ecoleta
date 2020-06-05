a0330b86f9d9747218e4f2fe63b84e4ea2b16529
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-06-05 00:48:39 -0300

    feat: screens detail ,home and points

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,8 +1,106 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  TouchableOpacity,
+  Image,
+  Text,
+  SafeAreaView,
+  Linking,
+} from 'react-native';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+import MailComposer from 'expo-mail-composer';
+import styles from './styles';
+import api from '../../services/api';
+
+interface Params {
+  point_id: number;
+}
+
+interface Data {
+  point: {
+    image: string;
+    name: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  };
+  items: {
+    title: string;
+  }[];
+}
 
 const Detail = () => {
-  return <View />;
+  const navigation = useNavigation();
+  const route = useRoute();
+  const [data, setData] = useState<Data>({} as Data);
+
+  const routeParams = route.params as Params;
+
+  useEffect(() => {
+    api.get(`points/${routeParams.point_id}`).then((response) => {
+      setData(response.data);
+    });
+  });
+  const handleNavigateBack = () => {
+    navigation.goBack();
+  };
+
+  const handleSendWhatsapp = () => {
+    Linking.openURL(
+      `whatsapp://send?phone=${data.point.whatsapp}&text=Tenho enteresse na coleta de residuos`,
+    );
+  };
+  const handleComposeMail = () => {
+    MailComposer.composeAsync({
+      subject: 'Interesse na coleta de resíduos',
+      recipients: [data.point.email],
+    });
+  };
+  if (!data.point) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Image
+          style={styles.pointImage}
+          source={{
+            uri: data.point.image,
+          }}
+        />
+        <Text style={styles.pointName}>{data.point.name}</Text>
+        <Text style={styles.pointItems}>
+          {data.items.map((item) => item.title).join(', ')}
+        </Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>
+            {data.point.city},{data.point.uf}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleSendWhatsapp}>
+          <FontAwesome name="whatsapp" size={20} color="#FFF" />
+          <Text style={styles.button}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={handleComposeMail}>
+          <FontAwesome name="mail" size={20} color="#FFF" />
+          <Text style={styles.button}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Detail;
--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,34 +1,75 @@
-import React from "react";
-import { Feather as Icon } from "@expo/vector-icons";
-import { View, Image, Text, ImageBackground } from "react-native";
-import { RectButton } from "react-native-gesture-handler";
-import styles from "./styles";
+import React, { useState } from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
+import {
+  View,
+  Image,
+  Text,
+  ImageBackground,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import styles from './styles';
 
 const Home: React.FC = () => {
+  const [uf, setUf] = useState('');
+  const [city, setCity] = useState('');
+  const navigation = useNavigation();
+  const handleNavigateToPoints = () => {
+    navigation.navigate('Points', { uf, city });
+  };
   return (
-    <ImageBackground
-      source={require("../../assets/home-background.png")}
-      style={styles.container}
-      imageStyle={{ width: 274, height: 368 }}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     >
-      <View style={styles.main}>
-        <Image source={require("../../assets/logo.png")} />
-        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-        <Text style={styles.description}>
-          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
-        </Text>
-      </View>
-      <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={() => {}}>
-          <View style={styles.buttonIcon}>
-            <Text>
-              <Icon name="arrow-right" color="#fff" size={24} />
+      <ImageBackground
+        source={require('../../assets/home-background.png')}
+        style={styles.container}
+        imageStyle={{ width: 274, height: 368 }}
+      >
+        <View style={styles.main}>
+          <Image source={require('../../assets/logo.png')} />
+          <View>
+            <Text style={styles.title}>
+              Seu marketplace de coleta de resíduos
+            </Text>
+            <Text style={styles.description}>
+              Ajudamos pessoas a encontrarem pontos de coleta de forma
+              eficiente.
             </Text>
           </View>
-          <Text style={styles.buttonText}>Entrar</Text>
-        </RectButton>
-      </View>
-    </ImageBackground>
+        </View>
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.input}
+            value={uf}
+            maxLength={2}
+            autoCapitalize="characters"
+            autoCorrect={false}
+            onChangeText={(text) => setUf(text)}
+            placeholder="Digite a uf"
+          />
+          <TextInput
+            value={city}
+            onChangeText={(text) => setCity(text)}
+            style={styles.input}
+            autoCorrect={false}
+            placeholder="Digite o cidade"
+          />
+          <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+            <View style={styles.buttonIcon}>
+              <Text>
+                <Icon name="arrow-right" color="#fff" size={24} />
+              </Text>
+            </View>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </RectButton>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
